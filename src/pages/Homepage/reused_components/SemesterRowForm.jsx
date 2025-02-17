c6c7e2ed008b3b8f6288f1ebc9ca5e46ede2dc1f
JRBuizon <buizonjeremiah@gmail.com>
2025-02-17 22:44:08 +0800

    feat: dropdown for school courses page

--- src/pages/Homepage/reused_components/SemesterRowForm.jsx
+++ src/pages/Homepage/reused_components/SemesterRowForm.jsx
@@ -8,23 +8,29 @@ const SemesterRowForm = ({ sessionId }) => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = () => {};
+  const onSubmit = () => { };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="w-full flex p-[0.5rem] gap-[1rem]">
         <div className="flex-1">
-          <input
-            className="w-full p-[0.2rem] rounded-md border-[1px] border-gray-200"
+          <span className="text-xs">Semester #</span>
+          <select
             {...register("semesterType", {
-              required: "Please enter a name.",
+              required: "Please select a semester type.",
             })}
-          />
+            className="w-full p-[0.2rem] rounded-md border-[1px] border-gray-200 text-sm"
+          >
+            <option>1</option>
+            <option>2</option>
+          </select>
         </div>
 
         <div className="flex-1">
+          <span className="text-xs">Start Date</span>
           <input
-            className="w-full p-[0.2rem] rounded-md border-[1px] border-gray-200"
+            type="date"
+            className="w-full p-[0.2rem] rounded-md border-[1px] border-gray-200 text-sm"
             {...register("startDate", {
               required: "Please enter a start date.",
             })}
@@ -32,8 +38,10 @@ const SemesterRowForm = ({ sessionId }) => {
         </div>
 
         <div className="flex-1">
+          <span className="text-xs">End Date</span>
           <input
-            className="w-full p-[0.2rem] rounded-md border-[1px] border-gray-200"
+            type="date"
+            className="w-full p-[0.2rem] rounded-md border-[1px] border-gray-200 text-sm"
             {...register("endDate", {})}
           />
         </div>
